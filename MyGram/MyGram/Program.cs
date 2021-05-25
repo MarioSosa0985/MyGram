0d643b5d8c45a0e087f439f7815fa3d814c08d8c
Mario Sosa <jsosa@southwestuniversity.edu>
2021-05-25 11:41:56 -0600

    This is my sixth assignment

--- MyGram/MyGram/Program.cs
+++ MyGram/MyGram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using MyGram.Models;
 
 namespace MyGram
@@ -7,14 +8,39 @@ namespace MyGram
     {
         static void Main(string[] args)
         {
-            int result = 0;
+            int[] myArray = new int[10];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 54324;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
+            myArray[9] = 10;
 
-            for(var i=1;i<=20;i++)
+            for(var i=0;i< myArray.Length;i++)
             {
-                result = result + i;
+                Console.WriteLine(myArray[i]);
             }
 
-            Console.WriteLine(result);
+            List<string> myList = new List<string>();
+            myList.Add("1");
+            myList.Add("2");
+            myList.Add("3");
+            myList.Add("4");
+            myList.Add("5");
+            myList.Add("6");
+            myList.Add("7");
+            myList.Add("8");
+            myList.Add("9");
+            myList.Add("10");
+
+            foreach(var item in myList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
