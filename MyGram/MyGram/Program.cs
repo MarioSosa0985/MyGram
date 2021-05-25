64dd47aa8236888dba1749dabdac15519a199003
Mario Sosa <jsosa@southwestuniversity.edu>
2021-05-25 10:17:56 -0600

    This is my first assignment

---
+++ MyGram/MyGram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyGram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintText();
+        }
+
+        public static void PrintText()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
+        }
+    }
+}
