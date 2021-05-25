2ac2da77a350bb371d335f581fa67572eeae6cdd
Mario Sosa <jsosa@southwestuniversity.edu>
2021-05-25 10:42:36 -0600

    This is my second assignment

--- MyGram/MyGram/Program.cs
+++ MyGram/MyGram/Program.cs
@@ -6,14 +6,42 @@ namespace MyGram
     {
         static void Main(string[] args)
         {
-            PrintText();
-        }
+            Users myUser = new Users();
+            myUser.UserId = 1;
+            myUser.UserName = "Mario Sosa";
 
-        public static void PrintText()
-        {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World again!");
+            Console.WriteLine(myUser.UserId);
+            Console.WriteLine(myUser.UserName);
         }
     }
+}
+
+public class Users
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+}
+
+public class Roles
+{
+    public int RoleId { get; set; }
+    public string RoleName { get; set; }
+}
+
+public class Images
+{
+    public int ImageId { get; set; }
+    public string ImagePath { get; set; }
+}
+
+public class Posts
+{
+    public int PostId { get; set; }
+    public string PostDate { get; set; }
+}
+
+public class Comments
+{
+    public int CommentId { get; set; }
+    public string TheComment { get; set; }
 }
