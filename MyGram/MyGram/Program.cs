b6a4b2dc5e09e69632f143398db5fa0254b2bdf5
Mario Sosa <jsosa@southwestuniversity.edu>
2021-05-25 11:29:58 -0600

    thi is fifth asignment

--- MyGram/MyGram/Program.cs
+++ MyGram/MyGram/Program.cs
@@ -7,17 +7,14 @@ namespace MyGram
     {
         static void Main(string[] args)
         {
-            DateTime myFirstDate = new DateTime(2003, 7, 15);
-            DateTime mySecondDate = new DateTime(2005, 2, 8);
+            int result = 0;
 
-            if(myFirstDate>mySecondDate)
+            for(var i=1;i<=20;i++)
             {
-                Console.WriteLine("First date is greater than the second date");
+                result = result + i;
             }
-            else
-            {
-                Console.WriteLine("First date is lower than the second date");
-            }
+
+            Console.WriteLine(result);
         }
     }
 }
